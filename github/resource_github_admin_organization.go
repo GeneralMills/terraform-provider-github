ca7d88036de6bf181783824ac92376ce107b3cb8
Erik Elkins <erik.elkins@outlook.com>
2020-05-25 16:27:54 -0500

    Adding org

---
+++ github/resource_github_admin_organization.go
@@ -0,0 +1,60 @@
+package github
+
+import (
+	"context"
+
+	"github.com/google/go-github/v31/github"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceGithubAdminOrganization() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceGithubAdminOrganizationCreate,
+		Update: resourceGithubAdminOrganizationUpdate,
+
+		Schema: map[string]*schema.Schema{
+			"admin": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+	}
+}
+
+func resourceGithubAdminOrganizationCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*Organization).v3client
+
+	admin := d.Get("admin").(string)
+	login := d.Get("login").(string)
+
+	input := &github.Organization{
+		Login: &login,
+	}
+
+	org, _, err := client.Admin.CreateOrg(context.Background(), input, admin)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(admin)
+	return org
+}
+
+func resourceGithubAdminOrganizationUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*Organization).v3client
+
+	admin := d.Get("admin").(string)
+	login := d.Get("login").(string)
+
+	input := &github.Organization{
+		Login: &login,
+	}
+
+	_, _, err := client.Admin.CreateOrg(context.Background(), input, admin)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(admin)
+	return org
+}
