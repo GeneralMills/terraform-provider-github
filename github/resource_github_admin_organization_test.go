28e78a617328beee057274ad62f480b0c63f48b5
Erik Elkins <erik.elkins@outlook.com>
2020-06-05 20:01:51 -0500

    Firist working test

--- github/resource_github_admin_organization_test.go
+++ github/resource_github_admin_organization_test.go
@@ -1,9 +1,7 @@
 package github
 
 import (
-	"context"
 	"fmt"
-	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
@@ -12,45 +10,31 @@ import (
 
 func TestAccGithubAdminOrg_basic(t *testing.T) {
 
-	secretResourceName := "resource_github_admin_organization.test_org"
-	secretValue := "super_secret_value"
-	updatedSecretValue := "updated_super_secret_value"
-	t.Log(testAccGithubActionsSecretFullConfig(repo, secretValue))
+	orgResourceName := "resource_github_admin_organization.test_org"
+	adminValue := "super_secret_value"
+	updatedAdminValue := "updated_super_secret_value"
+	t.Log(testAccGithubAdminOrgConfig(adminValue))
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckGithubActionsSecretDestroy,
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccGithubActionsSecretFullConfig(repo, secretValue),
+				Config: testAccGithubAdminOrgConfig(adminValue),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckGithubActionsSecretExists(secretResourceName, "test_secret_name", t),
-					resource.TestCheckResourceAttr("github_actions_secret.test_secret", "plaintext_value", secretValue),
-					resource.TestCheckResourceAttrSet("github_actions_secret.test_secret", "created_at"),
-					resource.TestCheckResourceAttrSet("github_actions_secret.test_secret", "updated_at"),
+					testAccCheckGithubAdminOrgExists(orgResourceName, "test_admin_name", t),
+					resource.TestCheckResourceAttr("github_admin_org.test_org", "admin", adminValue),
 				),
 			},
 			{
-				Config: testAccGithubActionsSecretFullConfig(repo, updatedSecretValue),
+				Config: testAccGithubAdminOrgConfig(updatedAdminValue),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckGithubActionsSecretExists(secretResourceName, "test_secret_name", t),
-					resource.TestCheckResourceAttr("github_actions_secret.test_secret", "plaintext_value", updatedSecretValue),
-					resource.TestCheckResourceAttrSet("github_actions_secret.test_secret", "created_at"),
-					resource.TestCheckResourceAttrSet("github_actions_secret.test_secret", "updated_at"),
+					testAccCheckGithubAdminOrgExists(orgResourceName, "test_admin_name", t),
+					resource.TestCheckResourceAttr("github_admin_org.test_org", "admin", updatedAdminValue),
 				),
 			},
 		},
 	})
-}
-
-func testAccGithubActionsSecretFullConfig(repoName, plaintext string) string {
-
-	// Take resources from other tests to avoid manual creation of secrets / repos
-	githubPKData := testAccCheckGithubActionsPublicKeyDataSourceConfig(repoName)
-	githubActionsSecretResource := testAccGithubAdminOrgConfig(repoName, plaintext)
-
-	return fmt.Sprintf("%s%s", githubPKData, githubActionsSecretResource)
 }
 
 func testAccGithubAdminOrgConfig(admin string) string {
@@ -61,49 +45,10 @@ resource "github_admin_organization" "test_org" {
 `, admin)
 }
 
-func testAccCheckGithubActionsSecretExists(resourceName, secretName string, t *testing.T) resource.TestCheckFunc {
+func testAccCheckGithubAdminOrgExists(resourceName, adminName string, t *testing.T) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
-		actualResource, ok := s.RootModule().Resources[resourceName]
-		if !ok {
-			return fmt.Errorf("not Found: %s", resourceName)
-		}
-
-		repoName := actualResource.Primary.Attributes["repository"]
-		if repoName == "" {
-			return fmt.Errorf("no repo name is set")
-		}
-
-		org := testAccProvider.Meta().(*Organization)
-		conn := org.v3client
-		_, _, err := conn.Actions.GetSecret(context.TODO(), org.name, repoName, secretName)
-		if err != nil {
-			t.Log("Failed to get secret")
-			return err
-		}
+		// TODO: Make sure admin org exists
 
 		return nil
 	}
-}
-
-func testAccCheckGithubActionsSecretDestroy(s *terraform.State) error {
-	client := testAccProvider.Meta().(*Organization).v3client
-
-	for _, rs := range s.RootModule().Resources {
-		if rs.Type != "github_actions_secret" {
-			continue
-		}
-		owner := testAccProvider.Meta().(*Organization).name
-		repoName := rs.Primary.Attributes["repository"]
-		secretName := rs.Primary.Attributes["secret_name"]
-
-		gotSecret, resp, err := client.Actions.GetSecret(context.TODO(), owner, repoName, secretName)
-		if err == nil && gotSecret != nil && gotSecret.Name == secretName {
-			return fmt.Errorf("secret %s still exists", rs.Primary.ID)
-		}
-		if resp != nil && resp.StatusCode != 404 {
-			return err
-		}
-		return nil
-	}
-	return nil
 }
